407fc87c2cc7f396b70a7a29ae2a2789b75cc59d
집에가고싶다 <cmshzl0727@gmail.com>
2026-01-15 14:37:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ os.makedirs(settings["downloadPath"], exist_ok=True)
 def MessageInputCallback(path):
     SingleSend.MessageCollector(path, settings)
 def MessageMakerCallback(downloadPath):
-    MessageMaker.MessageMaker(downloadPath, settings, asyncSocketListener.SendBinary)
+    MessageMaker.MessageMaker(downloadPath, settings, asyncSocketListener.send)
 def FileInputCallback(path):
     FileAct.FileCollector(path, settings)
 try:
